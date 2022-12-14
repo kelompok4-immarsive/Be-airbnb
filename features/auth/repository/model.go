004bf19c7509689b73664351fa6f12bfd3f31c22
unknown <techmin7@gmail.com>
2022-12-14 14:12:52 +0700

    login v3

--- features/auth/repository/model.go
+++ features/auth/repository/model.go
@@ -2,29 +2,36 @@ package repository
 
 import (
 	"fajar/testing/features/auth"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Name     string `gorm:"type:varchar(50)"`
-	Password string `gorm:"type:varchar(500)"`
-	Email    string
-	Phone    string
-	Address  string
-	Status   string
-	Role     string
+	ID        uint
+	Name      string `gorm:"type:varchar(50)"`
+	Password  string `gorm:"type:varchar(500)"`
+	Email     string
+	Phone     string
+	Address   string
+	Status    string
+	Role      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
 }
 
 func (data User) ToCore() auth.CoreUser {
 	return auth.CoreUser{
-		Name:     data.Name,
-		Password: data.Password,
-		Email:    data.Email,
-		Phone:    data.Phone,
-		Address:  data.Address,
-		Role:     data.Role,
-		Status:   data.Status,
+		ID:        data.ID,
+		Name:      data.Name,
+		Password:  data.Password,
+		Email:     data.Email,
+		Phone:     data.Phone,
+		Address:   data.Address,
+		Role:      data.Role,
+		Status:    data.Status,
+		CreatedAt: data.CreatedAt,
+		UpdatedAt: data.UpdatedAt,
 	}
 }
